fee4d8547a814e58a4f9fd86d1c50f581797efd9
Lincolm Ferreira de Aguiar <lincolm@lincolmlabs.com.br>
2025-10-01 23:30:32 -0300

    Delete test01.R
    Signed-off-by: Lincolm Ferreira de Aguiar <lincolm@lincolmlabs.com.br>

--- test01.R
+++
@@ -1,9 +0,0 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("GEOquery")
-
-library(GEOquery)
-gse <- getGEO("GSE134900", GSEMatrix = TRUE)
-nexprSet <- exprs(gse[[1]])
-phenoData <- pData(gse[[1]])
-
